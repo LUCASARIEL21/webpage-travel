f27ed7694bd7db0b26e308441a2a50e7b31b23bd
LUCASARIEL21 <lukasbcunha@gmail.com>
2023-02-08 23:38:58 -0300

    colocando anuncios atualizado

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { GoogleAd } from "../components/Comp home/anuncios1"
+import { GoogleAd } from "../components/Comp home/adsense"
 import { Credits } from "../components/Comp home/creditos"
 import { NavBar } from "../components/Comp home/navbar"
 import { Carrosel } from "../components/Comp home/populares"
@@ -10,14 +10,7 @@ export const Home = () => {
     <div>
       <NavBar />
       <Carrosel />
-      <div style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: "15px"
-      }}>
-        <GoogleAd slot="1085787706" googleAdId="ca-pub-5729807167062648" />
-      </div>
+      <GoogleAd />
       <Estados />
       <Credits />
     </div>
